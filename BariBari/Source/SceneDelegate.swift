93c1ae6aa4492e7b211b3fca9cd80cdc26363694
Goohyun3436 <3436hyun@gmail.com>
2025-06-27 13:23:22 +0900

    fix: [위젯] Cold Launch 시 앱 진입 후 Deeplink 미적용 현상 #33

--- BariBari/Source/SceneDelegate.swift
+++ BariBari/Source/SceneDelegate.swift
@@ -8,10 +8,9 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -20,10 +19,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        
         window?.rootViewController = TabBarController()
-        
         window?.makeKeyAndVisible()
+        
+        if let url = connectionOptions.urlContexts.first?.url {
+            DeepLinkManager.shared.link(url)
+        }
     }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
